34ad9c64504b05eb3acdd516373b8442c6680417
Daniel Haase <daniel@MINT-3.MINTKOLLEG>
2015-06-10 15:59:36 +0200

    Initialer Commit fuer die Modulquelltexte

---
+++ module_veundmint/config.pl
@@ -0,0 +1,15 @@
+# Globale Einstellungen fuer mconvert.pl und conv.pl (sowie die darin eingebundenen modules)
+
+
+# VE&MINT-Onlinekurs
+nosols          =>      0       , # =0: Alle Loesungsumgebungen uebersetzen, =1: Loesungsumgebungen nicht uebersetzen wenn SolutionSelect-Pragma aktiviert ist
+doscorm         =>      0       , # =0: Kein SCORM, =1 -> SCORM-Manifest und Definitionsdateien miterzeugen, html-Baum kann dann als SCORM-Lernmodul Version 4 verwendet werden, dann muss auch entsprechendes Flag in conv.pl gesetzt werden
+qautoexport     =>      0       , # =1 Alle MExercise-Umgebungen werden auch als Export verpackt
+diaok           =>      1       , # =1 dia/convert-Kette durchfueren, wenn im Programmablauf auf 0 gesetzt wird dia/convert fuer alle files nicht mehr ausgefuehrt
+cleanup         =>      0       , # =1 -> trunk-Verzeichnis wird nach Erstellung entfernt (fuer Releases unbedingt aktivieren)
+localjax        =>      0       , # =1 -> lokales MathJax-Verzeichnis wird eingerichtet (andernfalls ist netservice-Flag in conv.pl erforderlich)
+borkify         =>      0       , # =1 html und js-Dateien werden borkifiziert
+dorelease       =>      0       , # In Release-Versionen werden z.B. bestimmte Logmeldungen unterdrueckt
+doverbose       =>      1       , # Schaltet alle Debugmeldungen auf der Browserkonsole an
+docollections   =>      0       , # Schaltet Export der collection-Exercises ein (schließt qautoexport und nosols aus)
+description     =>      "Standard"
