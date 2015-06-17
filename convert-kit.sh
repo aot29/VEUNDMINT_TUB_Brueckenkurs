80e8d1b5caba778d6ae9c452b0af87ddcc492b0e
Max Bruckner <max@maxbruckner.de>
2015-06-17 15:36:47 +0200

    convert.sh upgedated

--- convert-kit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $1 == 'pdf' ]]; then
-    OPTION='-treepdf'
-else
-    OPTION='-tree'
-fi
-
-./converter/mconvert.pl $OPTION module_veundmint/tree_kit.tex module_veundmint output
---
+++ convert.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./converter/mconvert.pl config.pl
