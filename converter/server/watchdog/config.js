19946ad692434b2ea6f10d5a1a7cb4765e136609
Max Bruckner <max@maxbruckner.de>
2015-06-24 17:09:16 +0200

    watchdog: Unterstützung beliebiger Requests

--- converter/server/watchdog/config.js
+++ converter/server/watchdog/config.js
@@ -1,9 +1,23 @@
 //config file for watchdog
 module.exports = {
   services: [
-    {name: 'feedback', url: 'http://localhost/mint/converter/server/feedback.php'},
-    {name: 'userdata', url: 'http://localhost/mint/converter/server/userdata.php'},
-    {name: 'course', url: 'http://localhost/mint/testhtml'}
+    {
+      name: 'feedback',
+      url: 'http://localhost/mint/converter/server/dbtest/feedback.php',
+      requests: [
+        {name: '1', method: 'POST', data: {feedback: 'watchdog'}, regex: new RegExp('^success')}
+      ]
+    },
+    {
+      name: 'userdata',
+      url: 'http://localhost/mint/converter/server/dbtest/userdata.php',
+      requests: []
+    },
+    {
+      name: 'course',
+      url: 'http://localhost/mint/testhtml',
+      requests: []
+    }
   ],
   //timeout in seconds
   timeout: 10,
--- converter/server/watchdog/watchdog.js
+++ converter/server/watchdog/watchdog.js
@@ -2,7 +2,7 @@
 'use strict'
 
 var ping = require('ping');
-var jquery = require('jquery');
+var request = require('request');
 var config = require('./config');
 
 //get information from a given URL
@@ -34,12 +34,6 @@ function timeout(result) {
   console.log(JSON.stringify(result));
 }
 
-function pingCallback(state, result, path) {
-  result[path] = result[path] ? result[path] : {};
-  result[path]['ping'] = state.alive;
-}
-
-
 //this function get's called in regular intervals
 function watch() {
   var result = {};
@@ -49,9 +43,34 @@ function watch() {
   config.services.forEach(
     function (service, index) {
       var urlInfo = getUrlInfo(service.url); //extract information from the URL
+
+      //defaut value 'false' for ping
+      result[service.name] = result[service.name] ? result[service.name] : {};
+      result[service.name]['ping'] = false;
+
+      //ping the host
       ping.promise.probe(urlInfo.host).then(
         function (state) {
-          pingCallback(state, result, service.name);
+          result[service.name] = result[service.name] ? result[service.name] : {};
+          result[service.name]['ping'] = state.alive;
+        }
+      );
+
+      //make requests
+      service.requests.forEach(
+        function (req, reqIndex) {
+          result[service.name]['requests'] = result[service.name]['requests'] 
+            ? result[service.name]['requests'] 
+            : {};
+          result[service.name]['requests'][req.name] = {response: false, success: false};
+          request[req.method.toLowerCase()](
+            service.url, {form: req.data}, function (error, response, body) {
+              if (!error) {
+                result[service.name]['requests'][req.name].response = true;
+                result[service.name]['requests'][req.name].success = req.regex.test(body);
+              }
+            }
+          );
         }
       );
     }
