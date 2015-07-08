8d92db2fd30c126155ec33195e4110476c007950
Max Bruckner <max@maxbruckner.de>
2015-07-08 14:40:33 +0200

    Server: PHP-Dateien unter GPL2 or later

--- converter/server/dbtest/feedback.php
+++ converter/server/dbtest/feedback.php
@@ -1,4 +1,23 @@
 <?php
+/* Copyright (C) 2015 KIT (www.kit.edu), Author: Max Bruckner (FSMaxB)
+ *
+ *     This file is part of the VE&MINT program compilation
+ *     (see www.ve-und-mint.de).
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 2 of the License,
+ *     or any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see http://www.gnu.org
+ */
+
 //load settings
 include_once 'feedback-config.php';
 
