753bf7523038bb9642ce8851b11e53437f04445c
Max Bruckner <max@maxbruckner.de>
2015-07-15 17:00:16 +0200

    server/feedback: Kommentare auf Englisch übersetzt

--- converter/server/dbtest/feedback.php
+++ converter/server/dbtest/feedback.php
@@ -24,14 +24,14 @@ include_once 'feedback-config.php';
 $feedback_string = $_POST["feedback"];
 if( !isset( $feedback_string ) || (gettype($feedback_string) != "string") ) {
 	$feedback_string = "";
-} else if( strlen($feedback_string) > $max_string_size ) { //String zu lang?
+} else if( strlen($feedback_string) > $max_string_size ) { //string too long?
 	exit( "error" );
 }
 
 $statistics_string = $_POST["statistics"];
 if( !isset( $statistics_string ) || (gettype($statistics_string) != "string") ) {
 	$statistics_string = "";
-} else if( strlen($statistics_string) > $max_string_size ) { //String zu lang?
+} else if( strlen($statistics_string) > $max_string_size ) { //string too long?
 	exit( "error" );
 }
 
@@ -43,42 +43,40 @@ $userip = ($_SERVER['X_FORWARDED_FOR']) ? $_SERVER['X_FORWARDED_FOR'] : $_SERVER
 
 if( $storage_backend === "database" ) {
 	try {
-		//Mit Datenbank verbinden
+		//connect to database
 		$database_handler = new PDO( "mysql:host=$mysql_hostname;dbname=$mysql_dbname;charset:utf8", $mysql_username, $mysql_password, array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ) );
 
-		//Hole liste aller Tabellen
+		//get list of all tables
 		$table_query = $database_handler->query("SHOW TABLES FROM $mysql_dbname");
 		$tables = $table_query->fetchAll(PDO::FETCH_NUM);
 		
-		//Tabelle $mysql_feedback_table anlegen, wenn sie noch nicht existiert
-		if( ! in_array( array( 0 => $mysql_feedback_table), $tables, TRUE /*auch Datentyp prüfen*/ ) ) {
-			//Tabelle erstellen
+		//create table $mysql_feedback_table if it doesn't exist already
+		if( ! in_array( array( 0 => $mysql_feedback_table), $tables, TRUE /*also check data type*/ ) ) {
 			$create_table = "CREATE TABLE $mysql_feedback_table ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 				content TEXT NOT NULL,
 				timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
 			$database_handler->exec( $create_table );
 		}
 
-		//Tabelle $mysql_statistics_table anlegen, wenn sie noch nicht existiert
-		if( ! in_array( array( 0 => $mysql_statistics_table), $tables, TRUE /*auch Datentyp prüfen*/ ) ) {
-			//Tabelle erstellen
+		//create table $mysql_statistics_table if it doesn't exist already
+		if( ! in_array( array( 0 => $mysql_statistics_table), $tables, TRUE /*also check data type*/ ) ) {
 			$create_table = "CREATE TABLE $mysql_statistics_table ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 				content TEXT NOT NULL,
 				timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
 			$database_handler->exec( $create_table );
 		}
 
-		//Wenn vorhanden, Feedback in Datenbank schreiben
+		//write feedback to database if there is any
 		if( $feedback_string !== "" ) {
-			$feedback_string = $database_handler->quote($feedback_string . " FROM $userip"); //Escapen um SQL-Injection zu verhindern
+			$feedback_string = $database_handler->quote($feedback_string . " FROM $userip"); //escape the feedback to prevent SQL injection
 			$database_handler->exec("INSERT INTO $mysql_feedback_table( content ) VALUES ($feedback_string)");
 			echo "success\n";
 			echo "$feedback_string\n";
 		}
 
-		//Wenn vorhanden, Feedback in Datenbank schreiben
+		//write statistics to database if there are any
 		if( $statistics_string !== "" ) {
-			$statistics_string = $database_handler->quote($statistics_string . " FROM $userip"); //Escapen um SQL-Injection zu verhindern
+			$statistics_string = $database_handler->quote($statistics_string . " FROM $userip"); //escape the statistics to prevent SQL injection
                         $database_handler->exec("INSERT INTO $mysql_statistics_table( content ) VALUES ($statistics_string)");
 			echo "success\n";
 			echo "$statistics_string\n";
@@ -86,23 +84,23 @@ if( $storage_backend === "database" ) {
 
 		
 		
-		//Datenbank-Verbindung schließen
+		//close database connection
 		$database_handler = null;
 		exit();
 	} catch( PDOException  $e ) {
 		$database_handler = null;
-		$storage_backend = "file";	//Fallback zur Speicherung in einer Datei
+		$storage_backend = "file"; //fallback to file backend in case of a database error
 	}
 }
 
-if( $storage_backend === "file" ) { //Kein elseif weil "file" der Fallback für "database" is
-	//Zeilentrenner entfernen ( sonst kann man keine Datensätze unterscheiden )
+if( $storage_backend === "file" ) { //no 'else if' because 'file' is the fallback
+	//remove line separators to make datasets distinguishible (only one line per dataset)
 	$feedback_string = str_replace( array( "\n", "\r\n" ), '', $feedback_string );
 	$statistics_string = str_replace( array( "\n", "\r\n" ), '', $statistics_string );
 
 
         
-	//Timestamp holen
+	//get current time
 	$timestamp = time();
 
 	$data = "";
@@ -113,10 +111,10 @@ if( $storage_backend === "file" ) { //Kein elseif weil "file" der Fallback für 
 		$data .= "STATISTICS,$timestamp: $statistics_string, FROM $userip\n";
 	}
 
-	/* Schreiben $data in die Datei. LOCK_EX sorgt dafür, dass hierbei keine race conditions auftreten
-	 * file_put_contents wartet, bis es sich ein exclusive lock holen kann, schreibt dann in die Datei und gibt das
-	 * lock dann wieder frei! Deswegen können beliebig viele Anfragen an den Server eintreffen, ohne dass zwei 
-	 * Skripte gleichzeitig in die Datei schreiben
+	/* Write $data to the file. LOCK_EX is used to prevent race conditions because file_put_contents
+	 * blocks until it can get an exclusive lock, then writes to the file and removes the lock.
+	 * This means that two scripts can run simultaneously without messing up lines by accessing the
+	 * file simultaneously.
 	 **/
 
 			
