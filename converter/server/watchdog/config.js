6881f9a9379c4a95f14e37cc0114f7e07d313feb
Max Bruckner <max@maxbruckner.de>
2015-10-09 17:59:44 +0200

    watchdog: Update config file

--- converter/server/watchdog/config.js
+++ converter/server/watchdog/config.js
@@ -19,14 +19,54 @@ module.exports = {
     },
     {
       name: 'userdata',
-      url: 'http://mintlx3.scc.kit.edu/dbtest/userdata.php',
+      url: 'http://mintlx3.scc.kit.edu/dbtest/userdata.php?session_id=WATCHDOG',
       ping: true,
       notify: true,
       requests: [
+        /*
+         * This logs in to the user database as user "WATCHDOG" with password "WATCHDOG" and
+         * checks if it can write data to the database.
+         */
         {
-          name: 'check_user', method: 'GET', data: {action: 'check_user'},
-          test: function (response, reqObject) {
-            return JSON.parse(response).status === true;
+          name: 'check_user', method: 'GET', data: {action: 'check_user', username: "WATCHDOG"},
+          test: function (response, reqObject, log) {
+            try {
+              var responseObject = JSON.parse(response);
+            } catch (exception) {
+              return false;
+            }
+
+            if (responseObject.user_exists === false) {
+              console.log("WATCHDOG user doesn't exist in the database, you have to create (with password WATCHDOG)."); //TODO this doesn't get logged to the log or email
+              return false;
+            }
+            return responseObject.status === true;
+          },
+          threshold: 1000
+        },
+        {
+          name: 'login', method: 'POST', data: {action: 'login', username: "WATCHDOG", password: "WATCHDOG"},
+          test: function (response, reqObject, log) {
+            try {
+              var responseObject = JSON.parse(response);
+            } catch (exception) {
+              return false;
+            }
+
+            return responseObject.status === true;
+          },
+          threshold: 1000
+        },
+        {
+          name: 'write_data', method: 'POST', data: {action: 'write_data', username: "WATCHDOG", data: "WATCHDOG test data"},
+          test: function (response, reqObject, log) {
+            try {
+              var responseObject = JSON.parse(response);
+            } catch (exception) {
+              return false;
+            }
+
+            return responseObject.status === true;
           },
           threshold: 1000
         }
@@ -35,11 +75,12 @@ module.exports = {
     {
       name: 'course',
       ping: true,
+      notify: true,
       url: 'http://mintlx3.scc.kit.edu/veundmint_kit/mpl/3.1.html',
       requests: [
         {
           name: '1', method: 'GET', data: {}, test: function (response, reqObject) {
-            return true;
+            return reqObject.statusCode === 200;
           },
           threshold: 1000
         }
@@ -48,24 +89,26 @@ module.exports = {
     {
       name: 'mintlx1_ilias_login',
       ping: true,
+      notify: true,
       url: 'http://mintlx1.scc.kit.edu/ilias/login.php',
       requests: [
         {
           name: '1', method: 'GET', data: {}, test: function (response, reqObject) {
-            return true;
+            return reqObject.statusCode === 200;
           },
           threshold: 1000
         }
       ]
     },
     {
-      name: 'tester_dh',
+      name: 'test_mintlx3_online.html',
       ping: true,
+      notify: true,
       url: 'http://mintlx3.scc.kit.edu/test_mintlx3_alive.html',
       requests: [
         {
           name: '1', method: 'GET', data: {}, test: function (response, reqObject) {
-            return true;
+            return reqObject.statusCode === 200;
           },
           threshold: 1000
         }
