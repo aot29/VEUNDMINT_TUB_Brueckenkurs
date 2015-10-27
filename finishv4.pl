bb7b1bb15f924a39dcba0d86a693f04b381848cd
Daniel Haase <daniel.haase@kit.edu>
2015-10-27 12:31:16 +0100

    NOSCORM-Version

--- finishv4.pl
+++ finishv4.pl
@@ -1,12 +1,12 @@
 # Globale Einstellungen fuer mconvert.pl und conv.pl (sowie die darin eingebundenen modules)
 
-scormlogin      =>      1       , # =1 -> Alle Anmeldesysteme werden deaktiviert, und Teilnehmer ueber SCORM identifiziert
+scormlogin      =>      0       , # =1 -> Alle Anmeldesysteme werden deaktiviert, und Teilnehmer ueber SCORM identifiziert
 nosols          =>      0       , # =0: Alle Loesungsumgebungen uebersetzen, =1: Loesungsumgebungen nicht uebersetzen wenn SolutionSelect-Pragma aktiviert ist
-doscorm         =>      1       , # =0: Kein SCORM, =1 -> SCORM-Manifest und Definitionsdateien miterzeugen, html-Baum kann dann als SCORM-Lernmodul Version 4 verwendet werden, dann muss auch entsprechendes Flag in conv.pl gesetzt werden
+doscorm         =>      0       , # =0: Kein SCORM, =1 -> SCORM-Manifest und Definitionsdateien miterzeugen, html-Baum kann dann als SCORM-Lernmodul Version 4 verwendet werden, dann muss auch entsprechendes Flag in conv.pl gesetzt werden
 qautoexport     =>      0       , # =1 Alle MExercise-Umgebungen werden auch als Export verpackt
 diaok           =>      0       , # =1 dia/convert-Kette durchfueren, wenn im Programmablauf auf 0 gesetzt wird dia/convert fuer alle files nicht mehr ausgefuehrt
 cleanup         =>      1       , # =1 -> trunk-Verzeichnis wird nach Erstellung entfernt (fuer Releases unbedingt aktivieren)
-localjax        =>      0       , # =1 -> lokales MathJax-Verzeichnis wird eingerichtet (andernfalls ist netservice-Flag in conv.pl erforderlich)
+localjax        =>      1       , # =1 -> lokales MathJax-Verzeichnis wird eingerichtet (andernfalls ist netservice-Flag in conv.pl erforderlich)
 borkify         =>      1       , # =1 html und js-Dateien werden borkifiziert
 dorelease       =>      1       , # In Release-Versionen werden z.B. bestimmte Logmeldungen unterdrueckt
 doverbose       =>      0       , # Schaltet alle Debugmeldungen auf der Browserkonsole an
