4a1d00fa42e6846ab3913f9f66b07f526e3a353f
alvaro <alvaro@smultron.org>
2016-06-16 15:52:45 +0200

    Roulette Tests

--- src/tests/systemtests/RouletteTest.py
+++ src/tests/systemtests/RouletteTest.py
@@ -7,10 +7,21 @@ Testet die Roulette-Uebungen
 '''
 import unittest
 from tests.systemtests.AbstractSystemTest import AbstractSystemTest
-
+from selenium.webdriver.support.ui import WebDriverWait
 
 class RouletteTest( AbstractSystemTest ):
 
+
+    def testRouletteElement(self):
+        '''
+        Is the roulette exercise on te page?
+        '''
+        # navigate to chapter 1 section 1.2
+        self._navToChapter( "1", "1.2" )
+        # get the first roulette exercise on the page
+        roulette = self.browser.find_element_by_id( "ROULETTECONTAINER_VBKM01_FRACTIONTRAINING" )
+        self.assertTrue( roulette, "Roulettecontainer not found" )
+        
 
     def testLocale(self):
         '''
@@ -20,13 +31,67 @@ class RouletteTest( AbstractSystemTest ):
         self._navToChapter( "1", "1.2" )
         # get the first roulette exercise on the page
         roulette = self.browser.find_element_by_id( "ROULETTECONTAINER_VBKM01_FRACTIONTRAINING" )
-        self.assertTrue( roulette, "Roulettecontainer not found" )
         # Check that keywords use the correct locale
         exerciseText = roulette.text.lower()
         self.assertTrue( self.locale["roulette_new"].lower() in exerciseText )
         self.assertTrue( self.locale["module_solution"].lower() in exerciseText )
 
 
+    def testShowSolution(self):
+        '''
+        Is the solution shown after clicking on the hint button?
+        '''
+        # navigate to chapter 1 section 1.2
+        self._navToChapter( "1", "1.2" )
+        # get the first roulette exercise on the page
+        roulette = self.browser.find_element_by_id( "ROULETTECONTAINER_VBKM01_FRACTIONTRAINING" )
+        # get the first hint element
+        hintEl = self.browser.find_element_by_id( "MHint1" )
+        self.assertTrue( hintEl, "Roulettecontainer not found" )
+        
+        # Check hint is hidden at first
+        self.assertEqual( "display: none;", hintEl.get_attribute("style") , "Hint is not dispayed correctly")
+        # Call display the hint-button directly in Javascript
+        self.browser.execute_script( "toggle_hint('MHint1')" )
+        # Check hint is revealed
+        self.assertEqual( "display: block;", roulette.find_element_by_id( "MHint1" ).get_attribute("style") , "Hint is not dispayed correctly")
+        
+
+    def testRecognizeSolution(self):
+        '''
+        Is the correct solution recognized (marked in green)?
+        Is the wrong solution recognized (marked in red)?
+        '''
+        # navigate to chapter 1 section 1.2
+        self._navToChapter( "1", "1.2" )
+
+        # Call the hint-javascript to load the solution
+        self.browser.execute_script( "toggle_hint('MHint1')" )
+        
+        # These exercises are fractions. Get the numerator and denominator of the solution (the solution comes last) 
+        nomXpath = "//div[@id='MHint1']/descendant::span[@class='mjx-numerator'][last()]"
+        numerator = self.browser.find_element_by_xpath( nomXpath ).text
+        denXpath = "//div[@id='MHint1']/descendant::span[@class='mjx-denominator'][last()]"
+        denominator = self.browser.find_element_by_xpath( denXpath ).text
+
+        # get the input field
+        inputEl = self.browser.find_element_by_id( "QFELD_1.2.2.QF1" )
+        
+        # if input field is empty, icon should be "question mark"
+        inputEl.clear()
+        self.assertTrue( "questionmark" in self.browser.find_element_by_id( "QMQFELD_1.2.2.QF1" ).get_attribute( "src" ), "Answer is displaying the wrong image" )
+
+        # if answer is wrong, icon should be "false"
+        inputEl.send_keys( "1/0" )
+        self.assertTrue( "false" in self.browser.find_element_by_id( "QMQFELD_1.2.2.QF1" ).get_attribute( "src" ), "Answer is displaying the wrong image" )
+
+        # Put the solution in the input field: icon should be "right"
+        answer = "%s/%s" % ( numerator, denominator )
+        inputEl.clear()
+        inputEl.send_keys( answer )
+        self.assertTrue( "right" in self.browser.find_element_by_id( "QMQFELD_1.2.2.QF1" ).get_attribute( "src" ), "Answer is displaying the wrong image" )
+        
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
