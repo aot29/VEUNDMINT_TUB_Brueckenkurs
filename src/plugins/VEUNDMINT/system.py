57e4906e64ff6d0172fb8705290047f8951de7a1
Niklas P <contact@niklasplessing.net>
2016-07-26 15:24:12 +0200

    jajaja der gitlab runner

--- src/plugins/VEUNDMINT/system.py
+++ src/plugins/VEUNDMINT/system.py
@@ -107,8 +107,7 @@ class System(object):
                                 self._printMessage(self.BASHCOLORRED, "ERROR: Wrong error type " + lvl + " on conversion platform, message: " + msg)
                             else:
                                 if (lvl == self.FATALERROR):
-                                    import sys as real_sys
-                                    self._printMessage(self.BASHCOLORRED, "FATAL ERROR: " + msg + real_sys.last_traceback)
+                                    self._printMessage(self.BASHCOLORRED, "FATAL ERROR: " + msg)
                                     self._encode_print("Program aborted with error code 1")
                                     sys.exit(3) # highest possible error level
                                 else:
@@ -321,13 +320,12 @@ class System(object):
 
 
     # prints text on the console (which maybe does not understand utf8, so we encode output in ASCII), and only if not in quiet mode
-    # had to alter this as it was throwing errors on the gitlab testrunner
     def _encode_print(self, txt):
         if not self.beQuiet:
             if self.doEncodeASCII == 1:
                 print(txt.encode(encoding = "us-ascii", errors = "backslashreplace").decode("us-ascii"))
             else:
-                print(txt.encode('utf-8', 'ignore'))
+                print(txt)
 
 
     # ends the program, returning the maximum error level reached during execution
