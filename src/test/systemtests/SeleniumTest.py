cb491c1f6b5a1f789f5d95b409b1df1d8dc86d83
Niklas P <contact@niklasplessing.net>
2016-08-10 18:26:38 +0200

    try to fix mathjax bug

--- src/test/systemtests/SeleniumTest.py
+++ src/test/systemtests/SeleniumTest.py
@@ -49,8 +49,10 @@ class SeleniumTest(unittest.TestCase):
         # 		if (self.driver.current_url != BASE_URL):
         # 		# get the url from environment, otherwise from settings
         start_url = os.getenv('BASE_URL', BASE_URL)
-
-        self.driver.get( start_url + '?no_mathjax' )
+        if no_mathjax:
+            self.driver.get( start_url + '?no_mathjax' )
+        else:
+            self.driver.get( start_url )
 
     def _navToChapter(self, chapter, section=None, lang = "de", no_mathjax=False):
         '''
