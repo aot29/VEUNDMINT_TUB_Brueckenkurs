7010e11049f5157610ec9d91dbaf30f044631c5e
Niklas P <contact@niklasplessing.net>
2016-08-19 00:00:15 +0200

    removed bootstrap output dir + fixed fonts gulp + maybe tests working?

--- gulpfile.js/tasks/fonts.js
+++ gulpfile.js/tasks/fonts.js
@@ -3,14 +3,15 @@ if(!config.tasks.fonts) return
 
 var gulp         = require('gulp')
 var path         = require('path')
-
+var using        = require('gulp-using')
 
 var paths = {
   dest: path.join(config.root.dest, config.tasks.fonts.dest)
 }
 
 var fontsTask = function() {
-  return gulp.src(config.tasks.fonts.src, {base: './tu9onlinekurstest/fonts'})
+  return gulp.src(config.tasks.fonts.src)
+    .pipe(using({prefix:'Using font', path:'relative', color:'yellow', filesize:true}))
     .pipe(gulp.dest(paths.dest))
 }
 
