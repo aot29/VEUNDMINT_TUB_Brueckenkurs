ef352978e5ca3d019be9dd3078d02fcdf7a1b769
Alvaro Ortiz Troncoso <ortiz@math.tu-berlin.de>
2016-08-23 15:58:17 +0200

    test webdriver ist wieder phantomjs

--- src/test/systemtests/SeleniumTest.py
+++ src/test/systemtests/SeleniumTest.py
@@ -54,8 +54,8 @@ class SeleniumTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        #self.driver = webdriver.PhantomJS(executable_path=BASE_DIR + '/node_modules/phantomjs/lib/phantom/bin/phantomjs', service_log_path=BASE_DIR + '/ghostdriver.log')
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.PhantomJS(executable_path=BASE_DIR + '/node_modules/phantomjs/lib/phantom/bin/phantomjs', service_log_path=BASE_DIR + '/ghostdriver.log')
+        #self.driver = webdriver.Firefox()
         self.driver.set_window_size(1120, 550)
         self.driver.set_page_load_timeout(5)
         self.driver.implicitly_wait(5)
