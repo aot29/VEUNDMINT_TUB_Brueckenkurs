7b79a4663547e533a68943d727402e5a23a5544a
Niklas P <contact@niklasplessing.net>
2016-09-27 12:12:08 +0200

    scorm task now creates zip archive

--- gulpfile.js/tasks/scorm.js
+++ gulpfile.js/tasks/scorm.js
@@ -2,19 +2,34 @@ var config       = require('../config')
 
 var gulp         = require('gulp')
 var scormManifest = require('gulp-scorm-manifest')
+var zip = require('gulp-zip');
+var gulpSequence    = require('gulp-sequence')
 
-var scormTask = function () {
+var scormManifestTask = function () {
     gulp.src('./public/**')
     .pipe(scormManifest({
       version: '1.2',
-      courseId: 'Gulp101',
+      courseId: 'TOC-1',
       SCOtitle: 'Intro Title',
-      moduleTitle: 'Module Title',
+      moduleTitle: 'Onlinebrückenkurs Mathematik',
       launchPage: 'index.html',
       fileName: 'imsmanifest.xml'
     }))
     .pipe(gulp.dest('./public'))
 }
 
-gulp.task('scorm', scormTask)
-module.exports = scormTask
+var scormArchiveTask = function () {
+  gulp.src('**/*', {cwd: 'public/'})
+  .pipe(zip('scormModule.zip'))
+  .pipe(gulp.dest('./public'))
+}
+
+var scormTask = function() {
+  gulpSequence('scormManifest', 'scormArchive');
+}
+
+gulp.task('scormManifest', scormManifestTask);
+gulp.task('scormArchive', scormArchiveTask)
+
+gulp.task('scorm', ['scormManifest'], scormArchiveTask);
+module.exports = [scormArchiveTask, scormManifestTask];
