f51bc21def82faf462cb386f69abfb0a212ee5cf
Alvaro Ortiz Troncoso <ortiz@math.tu-berlin.de>
2016-11-08 15:54:11 +0100

    Removed Option.py and gulpfile.js, as these are course-specific (at least for now)

--- gulpfile.js/tasks/default.js
+++
@@ -1,9 +0,0 @@
-var gulp            = require('gulp')
-var gulpSequence    = require('gulp-sequence')
-
-var defaultTask = function(cb) {
-  gulpSequence('clean', ['fonts', 'images', 'scripts'], ['css', 'mathjax', 'html', 'scormTest'], 'inject', cb)
-}
-
-gulp.task('default', defaultTask)
-module.exports = defaultTask
